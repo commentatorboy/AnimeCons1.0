e578779bcc12728361190c8e4616c08b56f449c9
Thomas Goris <thomasgoris@hotmail.be>
2017-10-03 15:09:47 +0200

    Jquery-fied search.js

--- javascript/search.js
+++ javascript/search.js
@@ -1,9 +1,61 @@
-var searchbar = document.getElementById('mySearch');
+var searchbar = $('#mySearch');
+var searchlist = $('#searchoptions');
+var json = data;
+
+$(function() {
+  searchbar.keyup(checkKeyPress);
+});
+
+//check if keypress was an enter
+function checkKeyPress(event) {
+  //notice the of the "mySearch" it is the id of the input
+  if (event.keyCode == 13) {
+    loadResult(mySearch.value);
+  } else {
+    searchJSON(mySearch.value);
+  }
+}
+
+function loadResult(searchvalue) {
+  var regex = "/" + searchvalue + "/";
+  for( var item in Object.keys(jsonfile)) {
+    if (regex.test(item)) {
+      window.location.href = jsonfile[item];
+      return;
+    }
+  }
+}
+
+function searchJSON(searchvalue) {
+  var result = findKeys(searchvalue);
+  removeList();
+  if (searchvalue.length > 0) {
+    createList(result);
+  }
+}
+
+function findKeys(query) {
+  return json.conventions.filter(function(el) {
+    return el.id.toLowerCase().indexOf(query.toLowerCase()) > -1;
+  });
+}
+
+function removeList() {
+  searchlist.empty();
+}
+
+function createList(result) {
+  for (var i = 0; i < result.length; i++){
+    searchlist.append($('<li>').append(result[i].id));
+  }
+}
+
+/*var searchbar = document.getElementById('mySearch');
 var searchlist = document.getElementById("searchoptions");
 searchbar.onkeyup = checkKeyPress;
 var json = data;
 
-/* check if keypress was an enter */
+  //check if keypress was an enter
 function checkKeyPress(e) {
   //notice the of the "mySearch" it is the id of the input
   if (e.keyCode == 13) {
@@ -58,5 +110,4 @@ function createList(result) {
     searchlist.appendChild(entry);
 
   }
-
-}
+}*/
